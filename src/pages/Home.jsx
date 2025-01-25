25f9fba5e388fb571a526aa2cdb3446fe9353655
Isaac Opoku <isaacmanuopoku@gmail.com>
2025-01-25 00:17:45 +0000

    modified

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,9 @@ import Api from "../components/Api"
 const Home = () => {
   return (
     <>
-      <div className="grid grid-cols-2 gap-2 px-5 py-20  w-full font-bold text-4xl">
-        <Api />
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-5 px-5 md:px-[50px] py-20  w-full mx-auto font-bold text-4xl">
+        {/* <Api /> */}
+        hello
       </div>
     </>
   )
