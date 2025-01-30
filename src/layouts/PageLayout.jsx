64d810b4ccb60c6b880b288c4359d5f4fece92dc
Isaac Opoku <isaacmanuopoku@gmail.com>
2025-01-30 03:44:54 +0000

    modified

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import RootLayout from "./RootLayout";
-import Home from "../pages/Home";
 import PageNotFound from "../pages/PageNotFound";
 import Loading from "../components/Loading";
+import ListLoading from "../components/ListLoading";
 import SuspenseWrapper from "../components/SuspenseWrapper";
 const Register = React.lazy(() => import('../pages/Register'));
 const Login = React.lazy(() => import('../pages/Login'));
+const Reservations = React.lazy(() => import('../pages/Reservations'));
+const Home = React.lazy(() => import('../pages/Home'));
 
 import { 
   createBrowserRouter, 
@@ -14,22 +16,38 @@ import {
   Route, 
   RouterProvider 
 } from "react-router-dom";
-import ListLoading from "../components/ListLoading";
 
 function PageLayout() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<RootLayout />}>
-        <Route index element={<Home />} />
-        <Route
-          path="home"
+
+        <Route index  
           element={
             <SuspenseWrapper fallback={<ListLoading />}>
               <Home />
             </SuspenseWrapper>
           }
         />
+
+        <Route path="home" 
+          element={
+            <SuspenseWrapper fallback={<ListLoading />}>
+              <Home />
+            </SuspenseWrapper>
+          }
+        />
+
+        <Route
+          path="reservations"
+          element={
+            <SuspenseWrapper fallback={<Loading />}>
+              <Reservations />
+            </SuspenseWrapper>
+          }
+        />
+
         <Route
           path="register"
           element={
@@ -38,6 +56,7 @@ function PageLayout() {
             </SuspenseWrapper>
           }
         />
+
         <Route
           path="login"
           element={
@@ -46,7 +65,9 @@ function PageLayout() {
             </SuspenseWrapper>
           }
         />
+
         <Route path="*" element={<PageNotFound />} />
+
       </Route>
     )
   );
