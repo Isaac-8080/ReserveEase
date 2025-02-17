210b5229c30f04c031eebefe93b960b5badd3e13
Elkan <Elkan@GALAXY-BOOK>
2025-02-17 09:15:10 -0900

    commit

--- src/contexts/ReservationContext.jsx
+++ src/contexts/ReservationContext.jsx
@@ -4,12 +4,9 @@ const ReservationsContext = createContext();
 
 function ReservationProvider({children}) {
   const [reservationList, setReservationList] = useState([]);
-
-  // Add selected reservation to the list
   const handleAddList = useCallback((selectedList) => {
     setReservationList((prevList) => [...prevList, selectedList]);
   }, []);
-
   const handleDelete = (id) => {
     setReservationList(
       reservationList.filter((table) => table.tableId !== id)
